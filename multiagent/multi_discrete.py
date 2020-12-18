1ef63909497eff9babd3dd5b5497cee45fd1ad8c
pedram <pedramrabiee>
2020-12-18 17:13:36 -0500

    Remove prng import

--- multiagent/multi_discrete.py
+++ multiagent/multi_discrete.py
@@ -2,9 +2,7 @@
 # (https://github.com/openai/gym/blob/1fb81d4e3fb780ccf77fec731287ba07da35eb84/gym/spaces/multi_discrete.py)
 
 import numpy as np
-
 import gym
-from gym.spaces import prng
 
 class MultiDiscrete(gym.Space):
     """
@@ -30,7 +28,7 @@ class MultiDiscrete(gym.Space):
     def sample(self):
         """ Returns a array with one sample from each discrete action space """
         # For each row: round(random .* (max - min) + min, 0)
-        random_array = prng.np_random.rand(self.num_discrete_space)
+        random_array = np.random.RandomState().rand(self.num_discrete_space)
         return [int(x) for x in np.floor(np.multiply((self.high - self.low + 1.), random_array) + self.low)]
     def contains(self, x):
         return len(x) == self.num_discrete_space and (np.array(x) >= self.low).all() and (np.array(x) <= self.high).all()
